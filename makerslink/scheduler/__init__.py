7083ddf3e3e3a9e468a46e6ca23b69f2c9c5fce2
atrixiex <bobo.en@gmail.com>
2018-07-30 06:08:13 -0700

    Fixed that new EventInstances cannot be added if they have the same start and end time and are generated by the same Event.
    Started working on limiting saved objects so that you can only take them if the status<2 but is not finished.

--- makerslink/scheduler/__init__.py
+++ makerslink/scheduler/__init__.py
@@ -3,3 +3,5 @@ from .widgets import DatePicker, DateTimePicker
 
 forms.DateField.widget = DatePicker
 forms.DateTimeField.widget = DateTimePicker
+
+default_app_config ='scheduler.apps.SchedulerConfig'
