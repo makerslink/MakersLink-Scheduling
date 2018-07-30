fbf72289a91ca3c234657b5e41434ce8873dcf9a
Lars Lindholm <larlin@lysator.liu.se>
2018-07-30 15:59:52 +0200

    Changed to locale SV for datepicker to get first day of week to monday. Changed dateformat to remove seconds from datepicker.

--- makerslink/scheduler/widgets.py
+++ makerslink/scheduler/widgets.py
@@ -1,9 +1,9 @@
 from bootstrap_datepicker_plus import DateTimePickerInput
 
 class DateTimePicker(DateTimePickerInput):
-    format='%Y-%m-%d %H:%M:%S'
-    options={"sideBySide":True, "calendarWeeks":True,}
+    format='%Y-%m-%d %H:%M'
+    options={"sideBySide":True, "calendarWeeks":True, "locale":"sv"}
 
 class DatePicker(DateTimePickerInput):
     format='%Y-%m-%d'
-    options={"calendarWeeks":True}
+    options={"calendarWeeks":True, "locale":"sv"}
