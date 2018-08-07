3d9c51e0b21d9c7f3eceaf59213027d7483d8c8b
Lars Lindholm <larlin@lysator.liu.se>
2018-08-07 20:27:01 +0200

    Removed EMAIL_BACKEND command outside those controlled by enviroment.

--- makerslink/makerslink/settings.py
+++ makerslink/makerslink/settings.py
@@ -134,9 +134,6 @@ AUTH_USER_MODEL = "accounts.User"
 
 LOGIN_REDIRECT_URL = '/'
 
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'scheduling@makerslink.se'
