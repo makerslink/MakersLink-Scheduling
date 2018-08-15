fdb9f2724472500225088ab78af19a105e30c3d8
atrixiex <bobo.en@gmail.com>
2018-08-15 20:09:22 +0200

    Update tasks.py

--- makerslink/scheduler/tasks.py
+++ makerslink/scheduler/tasks.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from scheduler.models import EventInstance
 
+import logging
+logger = logging.getLogger(__name__)
+
 @shared_task
 def add(x, y):
     return x+y
@@ -10,7 +13,7 @@ def add(x, y):
 def create_cancellation_task(id, time):
     logger.warning('EventInstance should be cancelled at some point in time')
     # Schedule the actual cancellation
-    cancel_eventinstance.apply_async((id), eta=time)
+    cancel_eventinstance.apply_async(args=(id,), eta=time)
 
 @shared_task
 def cancel_eventinstance(id):
