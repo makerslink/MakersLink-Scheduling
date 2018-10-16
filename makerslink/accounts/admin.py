37c9cafae676096a667184b6a0c2bcda2572c942
Lars Lindholm <lars.lindholm@vtab.se>
2018-10-16 14:21:03 +0200

    Moved mar_approved into UserAdmin class. Added a nice message when action is completed.

--- makerslink/accounts/admin.py
+++ makerslink/accounts/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from .models import User
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-def make_approved(modeladmin, request, queryset):
-    queryset.update(is_active=True, is_registration_complete=True)
-make_approved.short_description = "Mark selected users as approved"
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -12,4 +10,13 @@ class UserAdmin(admin.ModelAdmin):
     form = CustomUserChangeForm
     model = User
     list_display = ('email', 'slackId', 'is_active', 'is_registration_complete')    
-    actions = [make_approved]
+    actions = ['make_approved']
+    
+    def make_approved(self, request, queryset):
+        rows_updated = queryset.update(is_active=True, is_registration_complete=True)
+        if rows_updated == 1:
+            message_bit = "1 host was"
+        else:
+            message_bit = "%s hosts were" % rows_updated
+        self.message_user(request, "%s successfully approved." % message_bit)
+    make_approved.short_description = "Mark selected users as approved"
