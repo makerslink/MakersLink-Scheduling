c22347fd8acc7b1a023fcf3317a0801b3f5bd902
atrixiex <bobo.en@gmail.com>
2019-04-14 16:53:40 +0200

    Added settings to include logging when DEBUG=True(dev-environment)

--- makerslink/makerslink/settings.py
+++ makerslink/makerslink/settings.py
@@ -37,6 +37,22 @@ else:
     # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = '!wdai3!c+cs7z!@w=27*b7(ggxi32!uw449=*ji+4m(mp#au+1'
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+            },
+        },
+        'loggers': {
+            'scheduler': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+            },
+        },
+    }
 
 # Application definition
 
