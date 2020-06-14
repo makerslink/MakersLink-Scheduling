c891277a11847c3637c55a452a2efdce035b1e01
Lars Lindholm <larlin@lysator.liu.se>
2020-06-14 15:09:53 +0200

    Switched to class based views for auth views.

--- makerslink/accounts/urls.py
+++ makerslink/accounts/urls.py
@@ -1,26 +1,23 @@
 from django.urls import path, re_path
 from django.conf.urls import url
 from .views import RegistrationView
-import django.contrib.auth.views
+import django.contrib.auth.views as authViews
 from . import views
 
 # User urls
 
 urlpatterns = [
-    path('login/', django.contrib.auth.views.login,
-        {'template_name': 'accounts/login.html'}, name='login'),
-    path('logout/', django.contrib.auth.views.logout,
-        {'template_name': 'accounts/logged_out.html'}, name='logout',),
+    path('login/', authViews.LoginView.as_view(template_name="accounts/login.html"),
+        name="login"),
+    path('logout/', authViews.LogoutView.as_view(template_name="accounts/logged_out.html"),
+        name='logout',),
     path('register/', RegistrationView.as_view(), name='register'),
-    path('register/done/', django.contrib.auth.views.password_reset_done,
-        {'template_name': 'accounts/register_done.html',}, name='register-done'),
+    path('register/done/', authViews.PasswordResetDoneView.as_view(template_name="accounts/register_done.html"), name='register-done'),
     re_path(r'^register/password/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 
-        django.contrib.auth.views.password_reset_confirm,{
-            'template_name': 'accounts/register_set_password.html',
-            'post_reset_redirect': 'register-complete',
-        }, name='password_set_confirm'),
-    path('register/complete/', django.contrib.auth.views.password_reset_complete, {
-        'template_name': 'accounts/register_complete.html',
-    }, name='register-complete'),
+        authViews.PasswordResetConfirmView.as_view(template_name="accounts/register_set_password.html",
+            success_url="register-complete"), name='password_set_confirm'),
+    path('register/complete/', authViews.PasswordResetCompleteView.as_view(
+        template_name="accounts/register_complete.html"),
+        name='register-complete'),
 ]
 
